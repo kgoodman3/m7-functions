9ad46972b9e4a1b4ccbd980c9ed29b29a1a73b98
Keenan Goodman <goodman3@D-108-179-189-213.dhcp4.washington.edu>
2017-04-05 17:43:25 -0700

    started off with a few scripts

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,7 +3,17 @@
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
+CompareLength <- function(vector1, vector2) {
+  N <- abs((length(vector1) - length(vector2)))
+  result <- "The difference is"
+  result.final <- paste(result, N)
+  (return) result
+}
 
+vector1 <- (10:20)
+vector2 <- (10:15)
+
+result <- CompareLength(vector1, vector2)
 # Pass two vectors of different length to your `CompareLength` function
 
 
